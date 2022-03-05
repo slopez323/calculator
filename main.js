ca8ad438449d3fe1cb1f498152ee55f0a09c8352
Sarah Lopez <slopez323@gmail.com>
2022-03-05 11:28:13 -0500

    keyboard support

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
 const operate = (operator, num1, num2) => {
-    return operator === 'add' ? add(num1, num2)
-        : operator === 'subtract' ? subtract(num1, num2)
-            : operator === 'multiply' ? multiply(num1, num2)
-                : operator === 'divide' ? divide(num1, num2)
+    return operator === 'Add' ? add(num1, num2)
+        : operator === 'Minus' ? subtract(num1, num2)
+            : operator === 'Multiply' ? multiply(num1, num2)
+                : operator === 'Slash' ? divide(num1, num2)
                     : 'ERROR'
 }
 
@@ -18,6 +18,9 @@ const calc = document.querySelector('.calculator');
 calc.addEventListener('click', run);
 calc.addEventListener('click', transition);
 calc.addEventListener('click', adjustFont);
+window.addEventListener('keydown', run);
+window.addEventListener('keydown', transition);
+window.addEventListener('keydown', adjustFont);
 const display = document.getElementById('display');
 const displayText = document.getElementById('displayText');
 const origFontSize = window.getComputedStyle(displayText, null).getPropertyValue('font-size');
@@ -26,25 +29,32 @@ let num1 = 0;
 let num2 = 0;
 let operator = '';
 let continueCalc = false;
-const digitButtons = ['digit0', 'digit1', 'digit2', 'digit3', 'digit4', 'digit5', 'digit6', 'digit7', 'digit8', 'digit9', 'dot'];
-const operatorButtons = ['add', 'subtract', 'multiply', 'divide'];
+const digitButtons = ['Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Period'];
+const operatorButtons = ['Add', 'Minus', 'Multiply', 'Slash'];
 
 function run(e) {
-    let btn = e.target.id;
-    if (btn === 'clear') {
+    let btn = e.target.id || e.code;
+    if(event.shiftKey === true){
+        if (btn === 'Equal'){
+            btn = 'Add'
+        } else if (btn === 'Digit8'){
+            btn = 'Multiply'
+        }
+    }
+    if (btn === 'clear' || btn === 'Escape') {
         displayText.textContent = '0';
         num = '';
         num1 = 0;
         num2 = 0;
         operator = '';
         displayText.style.fontSize = '';
-    } else if (btn === 'back') {
+    } else if (btn === 'Backspace') {
         num = num.substring(0, num.length - 1);
         if (num === '') {
             displayText.textContent = '0';
         } else displayText.textContent = num;
     } else if (digitButtons.includes(btn)) {
-        if (btn === 'dot') {
+        if (btn === 'Period') {
             if (!num.includes('.')) {
                 num += '.';
             }
@@ -71,7 +81,7 @@ function run(e) {
             num = '';
             operator = btn;
         }
-    } else if (btn === 'equal') {
+    } else if (btn === 'Equal' || btn === 'Enter') {
         if (operator === '') {
         } else if (num !== '') {
             num2 = +num;
@@ -91,10 +101,11 @@ function run(e) {
 }
 
 function transition(e) {
-    let clicked = document.getElementById(e.target.id);
+    let clicked = document.getElementById(e.target.id) || document.getElementById(e.code);
+    if (!clicked) return;
     clicked.style.opacity = '0.5'
     clicked.addEventListener('transitionend', function () {
-        clicked.style.opacity = '1'
+        clicked.style.opacity = ''
     })
 }
 
@@ -111,3 +122,5 @@ function adjustFont() {
         }
     }
 }
+
+
